f7c626a8677cef37046ecdeb9428e4af536433a1
Ivan5ik <lacoste545438@gmail.com>
2022-07-04 14:45:03 +0300

    create right form

--- src/components/inputName/index.tsx
+++ src/components/inputName/index.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { FC } from "react";
 import { Form, Input } from "antd";
 import { useTranslation } from "react-i18next";
 
-const InputName = () => {
-  const { t } = useTranslation();
+// const InputName = () => {
+//   const { t } = useTranslation();
 
-  return (
-    <Form.Item
-      label={t("form.name")}
-      name="name"
-      rules={[{ required: true, message: "Please input your name!" }]}
-    >
-      <Input />
-    </Form.Item>
-  );
+//   return (
+//     <Form.Item
+//       label={t("form.name")}
+//       name="name"
+//       rules={[{ required: true, message: "Please input your name!" }]}
+//     >
+//       <Input />
+//     </Form.Item>
+//   );
+// };
+// export { InputName };
+export const InputName = (placeholder: any) => {
+  return <Input placeholder={placeholder} />;
 };
-export { InputName };
